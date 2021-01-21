f9df8eed234189db1e542a3598943c88b67be4a9
Daniel Spieldenner <DanielSpieldenner@gmail.com>
2021-01-21 16:13:43 +0100

    Moved methods to separate Utils class for dealing with rdf4j models

--- poser-service/src/main/java/de/dfki/asr/poser/Converter/RdfToJson.java
+++ poser-service/src/main/java/de/dfki/asr/poser/Converter/RdfToJson.java
@@ -1,12 +1,8 @@
 package de.dfki.asr.poser.Converter;
 
+import de.dfki.asr.poser.util.RDFModelUtil;
 import java.util.ArrayList;
-import java.util.NoSuchElementException;
-import org.eclipse.rdf4j.model.IRI;
 import org.eclipse.rdf4j.model.Model;
-import org.eclipse.rdf4j.model.Resource;
-import org.eclipse.rdf4j.model.Statement;
-import org.eclipse.rdf4j.model.Value;
 import org.eclipse.rdf4j.model.ValueFactory;
 import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
 import org.json.JSONObject;
@@ -27,31 +23,16 @@ public class RdfToJson {
 		inputModel.setNamespace("json", "http://some.json.ontology/");
 		JSONObject jsonResult = new JSONObject();
 		// check the RDF description of the JSON API model for the desired input type
-		String inputType = getDesiredInputType(jsonModel);
+		String inputType = RDFModelUtil.getDesiredInputType(jsonModel);
 		// get the json object description that maps to this input type from the JSON model
 		jsonResult = buildJsonObjectFromDescriptionFile(inputType, jsonModel);
 		return jsonResult.toString();
 	}
 
-	/**
-	 * Get the Input Type the JSON API is expecting from the JSON Model file
-	 * @param jsonModel The RDF description of the JSON API
-	 * @return The expected input type
-	 */
-	private String getDesiredInputType(Model jsonModel) {
-		ValueFactory vf = SimpleValueFactory.getInstance();
-		IRI entryPoint = vf.createIRI("http://some.json.ontology/EntryPoint");
-		for(Statement typeDescription: jsonModel.filter(entryPoint, null, null)) {
-			Value typeObject = typeDescription.getObject();
-			return typeObject.stringValue();
-		}
-		throw new NoSuchElementException("No entry point description found in JSON model");
-	}
-
 	private JSONObject buildJsonObjectFromDescriptionFile(String jsonType, Model jsonModel) {
-		Model jsonObjectModel = getModelForJsonObject(jsonType, jsonModel);
+		Model jsonObjectModel = RDFModelUtil.getModelForJsonObject(jsonType, jsonModel);
 		JSONObject resultObject = new JSONObject();
-		String jsonKey = getKeyForObject(jsonObjectModel);
+		String jsonKey = RDFModelUtil.getKeyForObject(jsonObjectModel);
 		ArrayList<Object> values = getValuesForObject(jsonObjectModel);
 		for(Object value: values) {
 			if (isLiteral(value)) {
@@ -64,29 +45,6 @@ public class RdfToJson {
 			}
 		}
 		return resultObject;
-	}
-
-	/**
-	 * Get the semantic representation of the JSON Object to be constructed from the JSON modelfile
-	 * @param jsonType The type for which to build the JSON Object
-	 * @return The model describing the Json Object for the given type
-	 */
-	private Model getModelForJsonObject(String jsonType, Model jsonModel) {
-		ValueFactory vf = SimpleValueFactory.getInstance();
-		IRI predicateValueIri = vf.createIRI("http://some.json.ontology/value");
-		IRI objectJsonTypeIri = vf.createIRI(jsonType);
-		Model jsonObjectModel = jsonModel.filter(null, predicateValueIri, objectJsonTypeIri);
-		return jsonObjectModel;
-	}
-
-	/**
-	 * Given a semantic representation of a API JSONObject from the jsonModelFile, get the
-	 * value of the resource describing the key
-	 * @param jsonObjectModel The Model to query for the name of the JSON key
-	 * @return The Json key as String
-	 */
-	private String getKeyForObject(Model jsonObjectModel) {
-		throw new UnsupportedOperationException("Not supported yet.");
 	}
 
 	private ArrayList<Object> getValuesForObject(Model jsonObjectModel) {
--- poser-service/src/main/java/de/dfki/asr/poser/util/RDFModelUtil.java
+++ poser-service/src/main/java/de/dfki/asr/poser/util/RDFModelUtil.java
@@ -1,5 +1,62 @@
 package de.dfki.asr.poser.util;
 
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import org.eclipse.rdf4j.model.IRI;
+import org.eclipse.rdf4j.model.Literal;
+import org.eclipse.rdf4j.model.Model;
+import org.eclipse.rdf4j.model.Resource;
+import org.eclipse.rdf4j.model.Statement;
+import org.eclipse.rdf4j.model.Value;
+import org.eclipse.rdf4j.model.ValueFactory;
+import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
+import org.eclipse.rdf4j.model.util.Models;
+
 public class RDFModelUtil {
 
+	/**
+	 * Get the semantic representation of the JSON Object to be constructed from the JSON modelfile
+	 * @param jsonType The type for which to build the JSON Object
+	 * @return The model describing the Json Object for the given type
+	 */
+	public static Model getModelForJsonObject(String jsonType, Model jsonModel) {
+		ValueFactory vf = SimpleValueFactory.getInstance();
+		IRI predicateValueIri = vf.createIRI("http://some.json.ontology/value");
+		IRI objectJsonTypeIri = vf.createIRI(jsonType);
+		// get the subject of the model representing the data
+		Resource jsonObjectDescription = jsonModel.filter(null, predicateValueIri, objectJsonTypeIri).subjects().iterator().next();
+		Model jsonObjectModel = jsonModel.filter(jsonObjectDescription, null, null);
+		return jsonObjectModel;
+	}
+
+	/**
+	 * Get the Input Type the JSON API is expecting from the JSON Model file
+	 * @param jsonModel The RDF description of the JSON API
+	 * @return The expected input type
+	 */
+	public static String getDesiredInputType(Model jsonModel) {
+		ValueFactory vf = SimpleValueFactory.getInstance();
+		IRI entryPoint = vf.createIRI("http://some.json.ontology/EntryPoint");
+		for(Statement typeDescription: jsonModel.filter(entryPoint, null, null)) {
+			Value typeObject = typeDescription.getObject();
+			return typeObject.stringValue();
+		}
+		throw new NoSuchElementException("No entry point description found in JSON model");
+	}
+
+	/**
+	 * Given a semantic representation of a API JSONObject from the jsonModelFile, get the
+	 * value of the resource describing the key
+	 * @param jsonObjectModel The Model to query for the name of the JSON key
+	 * @return The Json key as String
+	 */
+	public static String getKeyForObject(Model jsonObjectModel) {
+		ValueFactory vf = SimpleValueFactory.getInstance();
+		IRI keyIri = vf.createIRI( "http://some.json.ontology/key");
+		Optional<Literal> key = Models.objectLiteral(jsonObjectModel.filter(null, keyIri, null));
+		if(key.isEmpty()) {
+			throw new NoSuchElementException("JSON Object " + jsonObjectModel.toString() + " is missing a key");
+		}
+		return key.get().stringValue();
+	}
 }
