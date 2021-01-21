678f3f45cb116bbdd29b3b98b99cd9c49151c844
Daniel Spieldenner <DanielSpieldenner@gmail.com>
2021-01-21 15:06:44 +0100

    New Application main class for better component scan

---
+++ poser-service/src/main/java/de/dfki/asr/poser/PoserApplication.java
@@ -0,0 +1,11 @@
+package de.dfki.asr.poser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoserApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PoserApplication.class, args);
+	}
+}
