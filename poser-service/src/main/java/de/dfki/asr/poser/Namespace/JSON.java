29da3f4de576ca93420f4d10f54396b08d9c53c9
Daniel Spieldenner <DanielSpieldenner@gmail.com>
2021-01-25 15:23:45 +0100

    Added a static convenience JSON namespace class

---
+++ poser-service/src/main/java/de/dfki/asr/poser/Namespace/JSON.java
@@ -0,0 +1,17 @@
+package de.dfki.asr.poser.Namespace;
+
+import org.eclipse.rdf4j.model.IRI;
+import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
+
+public class JSON {
+	public static final String NAMESPACE = "http://some.json.ontology/";
+	public static final String PREFIX = "json";
+	public static final IRI NUMBER = getIri("Number");
+	public static final IRI STRING = getIri("String");
+	public static final IRI OBJECT = getIri("Object");
+	public static final IRI BOOLEAN = getIri("Boolean");
+
+	private static IRI getIri(String localName) {
+		return SimpleValueFactory.getInstance().createIRI(NAMESPACE, localName);
+	}
+}
