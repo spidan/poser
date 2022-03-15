f9e5cdf42d93fdacbb7f331023edfb288c1f1b1e
Daniel Spieldenner <DanielSpieldenner@gmail.com>
2022-03-15 13:36:49 +0100

    Added simple example route

--- poser-service/src/main/java/de/dfki/asr/poser/PoserApplication.java
+++ poser-service/src/main/java/de/dfki/asr/poser/PoserApplication.java
@@ -2,10 +2,22 @@ package de.dfki.asr.poser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PoserApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PoserApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p
+					.path("/test")
+					.filters(f -> f.addRequestHeader("Hello", "World"))
+				.uri("http://httpbin.org:80")).build();
 	}
 }
