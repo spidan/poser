f6e13032db113b42ee513696e0719e6b2739035a
Daniel Spieldenner <DanielSpieldenner@gmail.com>
2022-03-15 15:06:41 +0100

    Implemented stub for simple GatewayFilter

--- poser-service/src/main/java/de/dfki/asr/poser/Converter/PoserGatewayFilterFactory.java
+++ poser-service/src/main/java/de/dfki/asr/poser/Converter/PoserGatewayFilterFactory.java
@@ -1,14 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package de.dfki.asr.poser.Converter;
 
-/**
- *
- * @author Daniel
- */
-public class PoserGatewayFilterFactory {
-	
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PoserGatewayFilterFactory extends AbstractGatewayFilterFactory<PoserGatewayFilterFactory.PoserGatewayConfig> {
+
+	public PoserGatewayFilterFactory() {
+        super(PoserGatewayConfig.class);
+    }
+
+	@Override
+	public GatewayFilter apply(PoserGatewayConfig config) {
+		return (exchange, chain) -> {
+			return chain.filter(exchange);
+			};
+	}
+
+	public static class PoserGatewayConfig {
+		public String transformToJSON() {
+			RdfToJson converter = new RdfToJson();
+			return "testomator";
+		}
+	}
 }
