2dc336a933c11f685df261b3e9168168c1fc5c99
Daniel Spieldenner <DanielSpieldenner@gmail.com>
2022-03-16 15:44:54 +0100

    Transform incoming request to JSON by calling the RdfToJson method

--- poser-service/src/main/java/de/dfki/asr/poser/Converter/PoserGatewayFilterFactory.java
+++ poser-service/src/main/java/de/dfki/asr/poser/Converter/PoserGatewayFilterFactory.java
@@ -1,8 +1,20 @@
 package de.dfki.asr.poser.Converter;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.commons.io.IOUtils;
+import org.apache.http.entity.ContentType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.cloud.gateway.filter.factory.rewrite.ModifyRequestBodyGatewayFilterFactory;
+import org.springframework.cloud.gateway.support.ServerWebExchangeUtils;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
 
 @Component
 public class PoserGatewayFilterFactory extends AbstractGatewayFilterFactory<PoserGatewayFilterFactory.Config> {
@@ -16,8 +28,21 @@ public class PoserGatewayFilterFactory extends AbstractGatewayFilterFactory<Pose
 	@Override
 	public GatewayFilter apply(Config config) {
 		return (exchange, chain) -> {
-			return chain.filter(exchange);
-			};
+			String cachedRequestBody = exchange.getAttribute(ServerWebExchangeUtils.CACHED_REQUEST_BODY_ATTR);
+			String loweringTemplateName = config.getLoweringTemplateName();
+			RdfToJson jsonConverter = new RdfToJson();
+			InputStream templateStream = this.getClass().getResourceAsStream("/".concat(loweringTemplateName));
+			try {
+				String loweringTemplate = IOUtils.toString(templateStream, Charset.forName("utf-8"));
+				String mappedRequestBody = jsonConverter.buildJsonString(cachedRequestBody, loweringTemplate);
+			ModifyRequestBodyGatewayFilterFactory.Config modifyRequestConfig = new ModifyRequestBodyGatewayFilterFactory.Config()
+                .setContentType(ContentType.APPLICATION_JSON.getMimeType()) // change content type ...
+                .setRewriteFunction(String.class, String.class, (oExchange, newRequestBody) -> Mono.just(mappedRequestBody));
+			return new ModifyRequestBodyGatewayFilterFactory().apply(modifyRequestConfig).filter(exchange, chain);
+			} catch (IOException ex) {
+				return chain.filter(exchange);
+			}
+		};
 	}
 
 	public static class Config {
