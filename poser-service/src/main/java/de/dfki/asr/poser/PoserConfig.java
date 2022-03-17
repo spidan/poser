8400c4d09d6a8a5be5dfa6104677c77314ac2d7c
Daniel Spieldenner <DanielSpieldenner@gmail.com>
2022-03-17 12:34:18 +0100

    Removed unused RDF2Json converter from route definition

--- poser-service/src/main/java/de/dfki/asr/poser/PoserConfig.java
+++ poser-service/src/main/java/de/dfki/asr/poser/PoserConfig.java
@@ -3,7 +3,6 @@ package de.dfki.asr.poser;
 import de.dfki.asr.poser.gatewayFilters.PoserGatewayFilterFactory;
 import java.net.URI;
 import java.util.Optional;
-import de.dfki.asr.poser.Converter.RdfToJson;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -16,7 +15,6 @@ public class PoserConfig {
 	@Bean
 	public RouteLocator myRoutes(RouteLocatorBuilder builder,
 			PoserGatewayFilterFactory poserFactory) {
-		RdfToJson jsonConverter = new RdfToJson();
 
 		return builder.routes()
 				.route(p -> p
