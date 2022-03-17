681b925623913fa0f105f062033d9e9d41d6bd6a
Daniel Spieldenner <DanielSpieldenner@gmail.com>
2022-03-17 15:37:38 +0100

    Added POST endpoint to store a template file locally in the service

--- poser-service/src/main/java/de/dfki/asr/poser/Controller/PoserControllers.java
+++ poser-service/src/main/java/de/dfki/asr/poser/Controller/PoserControllers.java
@@ -2,10 +2,16 @@ package de.dfki.asr.poser.Controller;
 
 import de.dfki.asr.poser.Converter.RdfToJson;
 import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import org.apache.commons.io.IOUtils;
 import org.eclipse.rdf4j.model.Model;
 import org.eclipse.rdf4j.model.impl.LinkedHashModel;
@@ -16,6 +22,8 @@ import org.eclipse.rdf4j.rio.RDFParser;
 import org.eclipse.rdf4j.rio.Rio;
 import org.eclipse.rdf4j.rio.UnsupportedRDFormatException;
 import org.eclipse.rdf4j.rio.helpers.StatementCollector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,6 +34,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PoserControllers {
+
+	private static final Logger LOG = LoggerFactory.getLogger(PoserControllers.class);
 
 	@GetMapping("/isAlive")
 	public String testService()
@@ -58,6 +68,30 @@ public class PoserControllers {
 		return new ResponseEntity<>(resultJson, HttpStatus.OK);
 	}
 
+	@PostMapping(path = "/storeTemplateFile",
+				consumes = "text/turtle")
+	public ResponseEntity<?> storeTemplateFile(@RequestBody final String template,
+												@RequestParam final String templateName) {
+		File templateFile = new File(getTemplateFileDir().concat(templateName));
+		if (!templateFile.getParentFile().mkdirs()) {
+			if (!Files.exists(Paths.get(getTemplateFileDir()))) {
+				return new ResponseEntity<>("Could not create mapping file folder",
+								HttpStatus.INTERNAL_SERVER_ERROR);
+			}
+		}
+		try (OutputStream outStream = new FileOutputStream(templateFile)) {
+			outStream.write(template.getBytes("utf-8"));
+			return new ResponseEntity<>("Worked", HttpStatus.OK);
+		} catch (FileNotFoundException ex) {
+			LOG.error("Could not open file: " + ex.getMessage());
+			return new ResponseEntity<>("Error opening file: " + ex.getMessage(), HttpStatus.NOT_FOUND);
+		} catch (IOException ex) {
+			LOG.error("Error saving file: " + ex.getMessage());
+			return new ResponseEntity<>("Error opening file: " + ex.getMessage(),
+							HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
 	private Model parseToTurtle(final String response) throws RDFHandlerException,
 	    UnsupportedRDFormatException, UnsupportedEncodingException, IOException, RDFParseException {
 	InputStream rdfStream = new ByteArrayInputStream(response.getBytes("utf-8"));
@@ -67,4 +101,10 @@ public class PoserControllers {
 	parser.parse(rdfStream);
 	return model;
     }
+
+	private String getTemplateFileDir() {
+		String workingDirPath = System.getProperty("user.dir");
+		String targetDirPath = workingDirPath.concat("/serviceTemplates/");
+		return targetDirPath;
+	}
 }
