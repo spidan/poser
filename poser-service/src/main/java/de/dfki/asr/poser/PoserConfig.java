26ee74a0b976878c1f44e2f311ccdf5c73df293f
Daniel Spieldenner <DanielSpieldenner@gmail.com>
2022-03-17 12:30:50 +0100

    Added filter step to read request uri from header

--- poser-service/src/main/java/de/dfki/asr/poser/PoserConfig.java
+++ poser-service/src/main/java/de/dfki/asr/poser/PoserConfig.java
@@ -1,11 +1,14 @@
 package de.dfki.asr.poser;
 
 import de.dfki.asr.poser.gatewayFilters.PoserGatewayFilterFactory;
+import java.net.URI;
+import java.util.Optional;
 import de.dfki.asr.poser.Converter.RdfToJson;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 
 @Configuration
 public class PoserConfig {
@@ -19,8 +22,14 @@ public class PoserConfig {
 				.route(p -> p
 					.path("/echo")
 					.filters(f -> f.cacheRequestBody(String.class)
-							.filter(poserFactory.apply(new PoserGatewayFilterFactory.Config("jsonApiMultipleValues.ttl"))) )
-				.uri("http://localhost:8123/"))
+							.filter(poserFactory.apply(new PoserGatewayFilterFactory.Config("jsonApiMultipleValues.ttl")))
+							.changeRequestUri(ex -> {
+								HttpHeaders headers = ex.getRequest().getHeaders();
+								URI locationUri = headers.getLocation();
+								return Optional.of(locationUri);
+							})
+					)
+				.uri("no://op"))
 				.build();
 	}
 }
